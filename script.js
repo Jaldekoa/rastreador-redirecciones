2b1f5a28788e2cf388e1aef9f746eca15827b934
Jon Aldekoa <jaldekoa@gmail.com>
2026-03-29 20:33:22 +0200

    Import and add functionality of button

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+import { trace, drawTable } from './main.js'
+
 const $input = document.getElementById('form__input');
 const $button = document.getElementById('form__button');
+const $table = document.getElementById('results-container');
 
-$button.addEventListener('click', (event) => {
+$button.addEventListener('click', async (event) => {
     event.preventDefault();
 
     const url = $input.value;
+    const redirectArr = await trace(url);
+    const resTable = drawTable(redirectArr);
 
-    // ¿...?
+    $table.replaceChildren(resTable);
 });
