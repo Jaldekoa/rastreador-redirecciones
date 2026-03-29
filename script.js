483a881116095b8f5e728084cd60ee854f303e1d
Jon Aldekoa <jaldekoa@gmail.com>
2026-03-29 17:10:27 +0200

    Add frontend script

---
+++ script.js
@@ -0,0 +1,10 @@
+const $input = document.getElementById('form__input');
+const $button = document.getElementById('form__button');
+
+$button.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const url = $input.value;
+
+    // ¿...?
+});
